f9c47ce120a83f78e41829de60f3ea844d6aca2c
unknown <sathwiknayak199@gmail.com>
2024-10-04 19:13:19 +0530

    Sitemap

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import sitemap from "vite-plugin-sitemap";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
       hostname: "https://sathwiknayak.vercel.app",
-      // other options can go here
     }),
   ],
 });
